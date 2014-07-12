dbdd05c56ac711231d8aae4bca91f124bfb26265
Ana Nelson <ana@ananelson.com>
2014-07-12 15:03:45 -0700

    Script tweaks.

--- run-dexy.sh
+++ run-dexy.sh
@@ -14,11 +14,16 @@ cmake ..                     \
 -DCMAKE_BUILD_TYPE=Release   \
 -DCMAKE_CXX_COMPILER=clang++ 
 make
+
+# Copy the spreading_drop_2d binary
 cp tests/sandbox/spreading_drop_2d/spreading_drop_2d ..
+
+# Copy the .py files (they should be moved so this is no longer necessary)
 cp tests/sandbox/spreading_drop_2d/*.py ..
 cp tests/tutorial/7_shallow_water/*.py ..
+
 popd
 chmod +x spreading_drop_2d
 
 ### "run-dexy"
-dexy -r -loglevel DEBUG
+dexy -loglevel DEBUG
